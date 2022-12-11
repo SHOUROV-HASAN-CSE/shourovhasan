98af50d2ab414bb4a4fa545a3ebecffea19c1a60
Shourov Hasan <shourovhasan172@gmail.com>
2022-12-10 20:42:39 -0800

    main branch created

---
+++ src/Pages/Error/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import error from '../../assets/images/error/error.webp';
+
+
+const Error = () => {
+  return (
+    <div className='flex justify-center'>
+      <img src={error} alt="" />
+    </div>
+  );
+};
+
+export default Error;
