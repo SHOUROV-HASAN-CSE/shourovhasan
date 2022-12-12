9ae69a046a8d684713b541542d7ad9dca80f743d
Shourov hasan <shourovhasan172@gmail.com>
2022-12-11 20:39:53 -0800

    particle complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import Particle from './Pages/config/Particle';
 import router from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className='max-w-screen-xl mx-auto'>
+      <Particle/>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
